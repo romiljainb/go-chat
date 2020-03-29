d637d1277b6a39c879321a34896b41b8fbd738f5
utsavjainb <utsavjainb@gmail.com>
2020-03-28 18:46:28 -0700

    Added username login, messages displayed with username

--- main.go
+++ main.go
@@ -8,10 +8,16 @@ import (
 	"strconv"
 )
 
+type client struct{
+    name string
+    ID int
+    uconn net.Conn
+}
 type message struct{
     msg string
-    sender int
+    sender client
 }
+
 
 var (
 	conns   = make(chan net.Conn)
@@ -19,6 +25,7 @@ var (
 	msgs    = make(chan message)
 	clients = make(map[net.Conn]int)
 	peers   = make(map[int]net.Conn)
+	users   = make(map[client]bool)
 	groups  = make(map[int][]net.Conn)
 )
 
@@ -35,7 +42,7 @@ func acceptConn(ln net.Listener) {
 }
 
 
-func readConn(conn net.Conn, i int) {
+func readConn(conn net.Conn, user client) {
 	rd := bufio.NewReader(conn)
 	for {
 		m, err := rd.ReadString('\n')
@@ -43,7 +50,7 @@ func readConn(conn net.Conn, i int) {
 			break
 		}
 
-        mdata := message{msg: m, sender: i}
+        mdata := message{msg: m, sender: user}
         msgs <- mdata
 	}
 	dconns <- conn
@@ -51,32 +58,64 @@ func readConn(conn net.Conn, i int) {
 }
 
 
-func handlePeer(data []string, info []string, sender int) {
+func handlePeer(data []string, info []string, sender client) {
     rec, err := strconv.Atoi(info[1])
     if err != nil {
         fmt.Println(err)
     }
 
     if rec <= 0 || rec > len(peers) {
-        peers[sender].Write([]byte("Reciever is out of range\n"))
+        peers[sender.ID].Write([]byte("Reciever is out of range\n"))
     } else {
 
-        msg := "Client " + strconv.Itoa(sender) + " : " + data[1] + "\n"
+        msg := sender.name + ": " + data[1] + "\n"
         peers[rec].Write([]byte(msg))
     }
 
 }
 
-func handleBroadcast(data []string, sender int){
-    msg := "Client " + strconv.Itoa(sender) + " : " + data[1] + "\n"
+func handleBroadcast(data []string, sender client){
+    msg := sender.name + ": " + data[1] + "\n"
     for conn := range clients {
         conn.Write([]byte(msg))
     }
 
 }
 
+func getUserDetails(conn net.Conn, id int) (client, bool){
+	rd := bufio.NewReader(conn)
+    var user client
+	for i:=0 ; i < 3; i++ {
+        conn.Write([]byte("Enter an username: \n"))
+		m, err := rd.ReadString('\n')
+		if err != nil {
+			break
+		}
+        pname := strings.TrimSpace(m)
+
+        if len(pname) > 0{
+            user = client{name : pname, ID: id, uconn: conn}
+            users[user] = true
+            return user, true
+        }
+
+	}
+	dconns <- conn
+    return user, false
+
+}
+
+func createUser(conn net.Conn, id int){
+    user, created := getUserDetails(conn, id)
+    if created {
+        clients[conn] = id
+        peers[id] = conn
+        go readConn(conn, user)
+    }
+
+}
 func handleConns() {
-	i := 0
+	i := 1
 
 	for {
 		select {
@@ -84,15 +123,12 @@ func handleConns() {
 		case conn := <-conns:
             _, exist := clients[conn]
             if !exist {
-                clients[conn] = i
+                createUser(conn, i)
                 i++
-                peers[i] = conn
-                go readConn(conn, i)
             }
 
 		// msg must be broadcast to everyone
 		case message := <-msgs:
-            fmt.Println(message.msg)
             if len(strings.TrimSpace(message.msg)) == 0{
                 continue
             }
@@ -106,7 +142,7 @@ func handleConns() {
 			} else if info[0] == "g" {
 
 			} else {
-				peers[message.sender].Write([]byte("Error parsing message info\n"))
+				peers[message.sender.ID].Write([]byte("Error parsing message info\n"))
 				fmt.Println("Error parsing message info")
 			}
 		case dconn := <-dconns:
